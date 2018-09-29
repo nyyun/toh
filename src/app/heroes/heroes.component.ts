636c049262bd74af775c40d6c7b0a9892963747e
김종혁 <zldpfm@naver.com>
2018-09-29 12:14:44 +0900

    부모 자식 연결

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -19,8 +20,14 @@ export class HeroesComponent implements OnInit {
   heroes = HEROES;
   selectedHero: Hero;
 
-  constructor() {
-
+  // 생성자로 이미 등록된 hero service 인스턴스를 주입 받는다.
+  //  => DI(dependancy Injection)
+  constructor(private heroService: HeroService) { // 선언만 하면 주입을 받는다.
+    // 의존성 있게 코딩하는 경우 (초보...ㅋㅋ)
+    // const heroService = new HeroService();
+    // this.heroes = heroService.getHeroes(); // 직접 호출!! 의존성 !!!
+   // private을 넣어주면 local 변수로 되기 때문에 this를 붙여줘도 된다.
+    this.heroes = this.heroService.getHeroes();
   }
 
   ngOnInit() {
